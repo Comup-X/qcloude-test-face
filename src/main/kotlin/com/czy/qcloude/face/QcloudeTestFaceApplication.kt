1a018f312dd40f6839ac704a26bd2b4be8614fb2
Comup <35429674@qq.com>
2017-07-03 20:07:49 +0800

    Initial commit

---
+++ src/main/kotlin/com/czy/qcloude/face/QcloudeTestFaceApplication.kt
@@ -0,0 +1,11 @@
+package com.czy.qcloude.face
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class QcloudeTestFaceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(QcloudeTestFaceApplication::class.java, *args)
+}
